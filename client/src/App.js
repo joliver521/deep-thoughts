2441e7b3b33bdab1d1cfbaaf7e47c0266bb072b2
Jason Oliver <58096660+joliver521@users.noreply.github.com>
2022-09-20 17:14:52 -0400

    build profile page

--- client/src/App.js
+++ client/src/App.js
@@ -11,6 +11,13 @@ import Footer from './components/Footer';
 
 import Home from './pages/Home';
 
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Login from './pages/Login';
+import NoMatch from './pages/NoMatch';
+import SingleThought from './pages/SingleThought';
+import Profile from './pages/Profile';
+import Signup from './pages/Signup';
+
 const httpLink = createHttpLink({
     uri: '/graphql',
 });
@@ -23,13 +30,26 @@ const client = new ApolloClient({
 function App() {
     return (
         <ApolloProvider client={client}>
-            <div className="flex-column justify-flex-start min-100-vh">
-                <Header />
-                <div className="container">
-                    <Home />
+            <Router>
+                <div className="flex-column justify-flex-start min-100-vh">
+                    <Header />
+                    <div className="container">
+                        <Routes>
+                            <Route path="/" element={<Home />} />
+                            <Route path="/login" element={<Login />} />
+                            <Route path="/signup" element={<Signup />} />
+                            <Route path="/profile" element={<Profile />} />
+                            <Route
+                                path="/thought/:id"
+                                element={<SingleThought />}
+                            />
+
+                            <Route path="*" element={<NoMatch />} />
+                        </Routes>
+                    </div>
+                    <Footer />
                 </div>
-                <Footer />
-            </div>
+            </Router>
         </ApolloProvider>
     );
 }
