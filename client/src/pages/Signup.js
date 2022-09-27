3c348be01de1e92a0c13ee3f3dcda515bd393654
Jason Oliver <58096660+joliver521@users.noreply.github.com>
2022-09-27 19:57:31 -0400

    updated package.json

--- client/src/pages/Signup.js
+++ client/src/pages/Signup.js
@@ -5,83 +5,83 @@ import { ADD_USER } from '../utils/mutations';
 import Auth from '../utils/auth';
 
 const Signup = () => {
-  const [formState, setFormState] = useState({
-    username: '',
-    email: '',
-    password: '',
-  });
-  const [addUser, { error }] = useMutation(ADD_USER);
-
-  // update state based on form input changes
-  const handleChange = (event) => {
-    const { name, value } = event.target;
-
-    setFormState({
-      ...formState,
-      [name]: value,
+    const [formState, setFormState] = useState({
+        username: '',
+        email: '',
+        password: '',
     });
-  };
+    const [addUser, { error }] = useMutation(ADD_USER);
 
-  // submit form
-  const handleFormSubmit = async (event) => {
-    event.preventDefault();
+    // update state based on form input changes
+    const handleChange = (event) => {
+        const { name, value } = event.target;
 
-    try {
-      const { data } = await addUser({
-        variables: { ...formState },
-      });
+        setFormState({
+            ...formState,
+            [name]: value,
+        });
+    };
 
-      Auth.login(data.addUser.token);
-    } catch (e) {
-      console.error(e);
-    }
-  };
+    // submit form
+    const handleFormSubmit = async (event) => {
+        event.preventDefault();
 
-  return (
-    <main className="flex-row justify-center mb-4">
-      <div className="col-12 col-md-6">
-        <div className="card">
-          <h4 className="card-header">Sign Up</h4>
-          <div className="card-body">
-            <form onSubmit={handleFormSubmit}>
-              <input
-                className="form-input"
-                placeholder="Your username"
-                name="username"
-                type="username"
-                id="username"
-                value={formState.username}
-                onChange={handleChange}
-              />
-              <input
-                className="form-input"
-                placeholder="Your email"
-                name="email"
-                type="email"
-                id="email"
-                value={formState.email}
-                onChange={handleChange}
-              />
-              <input
-                className="form-input"
-                placeholder="******"
-                name="password"
-                type="password"
-                id="password"
-                value={formState.password}
-                onChange={handleChange}
-              />
-              <button className="btn d-block w-100" type="submit">
-                Submit
-              </button>
-            </form>
+        try {
+            const { data } = await addUser({
+                variables: { ...formState },
+            });
 
-            {error && <div>Signup failed</div>}
-          </div>
-        </div>
-      </div>
-    </main>
-  );
+            Auth.login(data.addUser.token);
+        } catch (e) {
+            console.error(e);
+        }
+    };
+
+    return (
+        <main className="flex-row justify-center mb-4">
+            <div className="col-12 col-md-6">
+                <div className="card">
+                    <h4 className="card-header">Sign Up</h4>
+                    <div className="card-body">
+                        <form onSubmit={handleFormSubmit}>
+                            <input
+                                className="form-input"
+                                placeholder="Your username"
+                                name="username"
+                                type="username"
+                                id="username"
+                                value={formState.username}
+                                onChange={handleChange}
+                            />
+                            <input
+                                className="form-input"
+                                placeholder="Your email"
+                                name="email"
+                                type="email"
+                                id="email"
+                                value={formState.email}
+                                onChange={handleChange}
+                            />
+                            <input
+                                className="form-input"
+                                placeholder="******"
+                                name="password"
+                                type="password"
+                                id="password"
+                                value={formState.password}
+                                onChange={handleChange}
+                            />
+                            <button className="btn d-block w-100" type="submit">
+                                Submit
+                            </button>
+                        </form>
+
+                        {error && <div>Signup failed</div>}
+                    </div>
+                </div>
+            </div>
+        </main>
+    );
 };
 
 export default Signup;
--- server/models/Thought.js
+++ server/models/Thought.js
@@ -3,33 +3,33 @@ const reactionSchema = require('./Reaction');
 const dateFormat = require('../utils/dateFormat');
 
 const thoughtSchema = new Schema(
-  {
-    thoughtText: {
-      type: String,
-      required: 'You need to leave a thought!',
-      minlength: 1,
-      maxlength: 280
+    {
+        thoughtText: {
+            type: String,
+            required: 'You need to leave a thought!',
+            minlength: 1,
+            maxlength: 280,
+        },
+        createdAt: {
+            type: Date,
+            default: Date.now,
+            get: (timestamp) => dateFormat(timestamp),
+        },
+        username: {
+            type: String,
+            required: true,
+        },
+        reactions: [reactionSchema],
     },
-    createdAt: {
-      type: Date,
-      default: Date.now,
-      get: timestamp => dateFormat(timestamp)
-    },
-    username: {
-      type: String,
-      required: true
-    },
-    reactions: [reactionSchema]
-  },
-  {
-    toJSON: {
-      getters: true
+    {
+        toJSON: {
+            getters: true,
+        },
     }
-  }
 );
 
-thoughtSchema.virtual('reactionCount').get(function() {
-  return this.reactions.length;
+thoughtSchema.virtual('reactionCount').get(function () {
+    return this.reactions.length;
 });
 
 const Thought = model('Thought', thoughtSchema);
