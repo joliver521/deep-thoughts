46e42b362e4795a2d35fdf53696c1a9d3ad4ea63
Jason Oliver <58096660+joliver521@users.noreply.github.com>
2022-10-06 08:59:32 -0400

    updated package.json

--- server/models/Reaction.js
+++ server/models/Reaction.js
@@ -2,27 +2,27 @@ const { Schema } = require('mongoose');
 const dateFormat = require('../utils/dateFormat');
 
 const reactionSchema = new Schema(
-    {
-        reactionBody: {
-            type: String,
-            required: true,
-            maxlength: 280,
-        },
-        username: {
-            type: String,
-            required: true,
-        },
-        createdAt: {
-            type: Date,
-            default: Date.now,
-            get: (timestamp) => dateFormat(timestamp),
-        },
+  {
+    reactionBody: {
+      type: String,
+      required: true,
+      maxlength: 280
     },
-    {
-        toJSON: {
-            getters: true,
-        },
+    username: {
+      type: String,
+      required: true
+    },
+    createdAt: {
+      type: Date,
+      default: Date.now,
+      get: timestamp => dateFormat(timestamp)
     }
+  },
+  {
+    toJSON: {
+      getters: true
+    }
+  }
 );
 
 module.exports = reactionSchema;
